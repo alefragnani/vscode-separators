f293d637c8359840680f207fbdea3b6b510a30ae
Alessandro Fragnani <alefragnani@hotmail.com>
2020-11-02 10:54:10 -0300

    Add licence header to sources

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Alessandro Fragnani. All rights reserved.
+*  Licensed under the MIT License. See License.md in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
