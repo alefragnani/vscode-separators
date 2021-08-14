aa899f83e73e15841a5a6f65693e2e2050687881
Alessandro Fragnani <alefragnani@hotmail.com>
2021-08-14 00:20:49 +0000

    Add `Struct` symbol support

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@
 *  Licensed under the MIT License. See License.md in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-export const DEFAULT_ENABLED_SYMBOLS = [ "Classes", "Constructors", "Enums", "Functions", "Interfaces", "Methods", "Namespaces" ];
+export const DEFAULT_ENABLED_SYMBOLS = ["Classes", "Constructors", "Enums", "Functions", "Interfaces", "Methods", "Namespaces", "Structs"];
 
 export const DEFAULT_GREENISH_COLOR = "#65EAB9";
 
